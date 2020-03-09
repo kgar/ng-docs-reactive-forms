0092d8a1e1dc9140ccd6b312a8cf84e20c1d5659
Kevin Garner <kevin.garner@ssigroup.com>
2020-03-09 14:58:59 -0500

    finished going through the doc https://angular.io/guide/reactive-forms

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
@@ -7,12 +7,40 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./profile-editor.component.scss'],
 })
 export class ProfileEditorComponent {
-  profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: [''],
+    }),
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
   });
+
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) {}
 
   onSubmit() {
     console.warn(this.profileForm.value);
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street',
+      },
+    });
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
   }
 }
